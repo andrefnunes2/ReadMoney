c9897dc971df73ae09d1fb3473c3220676ef74e5
André <andre.nunes@ventron.com.br>
2018-10-04 16:41:06 -0300

    first commit

---
+++ ReadMoney/ReadMoney.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace ReadMoney
+{
+    partial class ReadMoney
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStop = new System.Windows.Forms.Button();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(180, 24);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(107, 23);
+            this.btnStop.TabIndex = 0;
+            this.btnStop.Text = "Parar analise";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(209, 86);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(35, 13);
+            this.lblInfo.TabIndex = 1;
+            this.lblInfo.Text = "label1";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(44, 24);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(130, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Iniciar analise";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // ReadMoney
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(299, 261);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.btnStop);
+            this.Name = "ReadMoney";
+            this.Text = "ReadMoney";
+            this.Load += new System.EventHandler(this.ReadMoney_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ ReadMoney/ReadMoney.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ReadMoney
+{
+    public partial class ReadMoney : Form
+    {
+        public ReadMoney()
+        {
+            InitializeComponent();
+        }
+        private bool GCloseRecursive;
+        private string GFilePath = @"D:\Projetos\Outros\ReadMoney\Files\PRINCIPAL-25-09.xlsm";
+
+        private int countTeste = 0;
+
+        private void ReadMoney_Load(object sender, EventArgs e)
+        {
+            
+        }
+       
+        private void AnalyzeFile(object sender, EventArgs e)
+        {
+            countTeste++;
+
+            Excel.Application xlApp = new Excel.Application();
+            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(GFilePath);
+            Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[8];
+            Excel.Range xlRange = xlWorksheet.UsedRange;
+
+            lblInfo.Text = countTeste.ToString() + "- " + xlRange.Cells[2, 2].Value2.ToString();
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            xlWorkbook.Close();
+            Marshal.ReleaseComObject(xlWorkbook);
+            xlApp.Quit();
+            Marshal.ReleaseComObject(xlApp);
+
+            if (!GCloseRecursive) AnalyzeFile(null, new EventArgs()); ;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            GCloseRecursive = true;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            GCloseRecursive = false;
+            AnalyzeFile(null, new EventArgs());
+        }
+    }
+}
